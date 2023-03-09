714b34942052a0a538a2d8af9b2d2431b26674c0
benjamin farmer <viroben2001@gmail.com>
2023-03-09 10:32:47 -0800

    inital first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { DocumentMagnifyingGlassIcon} from '@heroicons/react/24/outline'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <DocumentMagnifyingGlassIcon className=' h-64 w-64 text-lime-600'/>
+
+        <h1 className='text-3xl mt-2 text-center text-indigo-600 font-bold mb-5'>Welcome to the Amazon Web Scraper</h1>
+
+        <h2 className='text-lg italic text-center text-fuchsia-600'> I have gained knowlage with next.js and fun with creating this project</h2>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
